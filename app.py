445641059f84a4fcaabe7d84d28929bf153248fa
Arthur Rampani <arthurrampanicosta@gmail.com>
2025-04-22 10:33:10 -0300

    Programando as Rotas

--- app.py
+++ app.py
@@ -6,15 +6,23 @@ from data.conexao import Conexao
 
 app = Flask(__name__)
 
+situacao_lampada = ""
+
+# Para abrir a página HTML
 @app.route("/")
-@app.route("/dashboard")
+@app.route("/lampada")
 def pagina_dashboard():
-    return render_template("dashboard.html")
+    return render_template("pagina_lampada.html")
 
-@app.route("/lampada/ligada")
+# Rota post para o fotoressitor acessar e nela aparecer que a Lâmpada está ligada no HTML
+@app.route("/post/lampada/ligada"  , methods = ["POST"])
 def post_lampada_ligada():
-    return render_template("dashboard.html")
+    return render_template("pagina_lampada.html", situacao_lampada = "Lampada Está Ligada")
 
+# Rota post para o fotoressitor acessar e nela aparecer que a Desligada está ligada no HTML
+@app.route("/post/lampada/desligada"  , methods = ["POST"])
+def post_lampada_desligada():
+    return render_template("pagina_lampada.html", situacao_lampada = "lampada_desligada")
 
 # Para iniciar o app
 app.run()
