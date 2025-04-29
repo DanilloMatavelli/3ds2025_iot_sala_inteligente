ca7cd688a92da4eedeb732161147760de3032ccb
DanilloMatavelli <danillomatavelli26@gmail.com>
2025-04-29 08:48:30 -0300

    Fazendo o script para ficar carregar o site

---
+++ static/estado_lampada.js
@@ -0,0 +1,21 @@
+async function atualizarStatus() {
+    const resposta = await fetch('/get/estado_lampada');
+    const dados = await resposta.json();
+    const estado = dados.ESTADO_LAMPADA;
+
+    const statusLampada = document.getElementById('status');
+    const icone_Lampada = document.getElementById('icone');
+
+    if (estado === 'LIGADO') {
+        statusLampada.innerText = 'Ligada';
+        icone_Lampada.innerText = '💡';
+    }
+    else {
+        statusLampada.innerText = 'Desligada';
+        icone_Lampada.innerText = '💤';
+    }
+}
+
+// Atualizar a cada 2 segundos
+setInterval(atualizarStatus, 2000);
+window.onload = atualizarStatus;
